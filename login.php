7bbe439e05a2adeb58737a6cb6f6def117cf9bde
NikolaPatafta <44970174+NikolaPatafta@users.noreply.github.com>
2024-02-23 10:54:31 +0100

    Registration & Login completed
    - registration & login working with SQL database

--- login.php
+++ login.php
@@ -1,5 +1,8 @@
 <?php
-    session_start();
+session_start();
+if(isset($_SESSION["user"])){
+    header("Location: index.php");
+}
 ?>
 
 <!DOCTYPE html>
@@ -7,31 +10,48 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href = "style.css">
 </head>
 <body>
-<form action = "index.php" method = "post">
-        <h2> Welcome to BookApp </h2>
-        Email:<br>
-        <input type = "email" name= "email"> <br>
-        Password: <br>
-        <input type = "password" name = "password"> <br>
-        <input type = "submit" name = "submit" value = "login">
-    </form>   
+    <div class ="container">
+        <?php
+            if(isset($_POST["login"])){
+                $email = $_POST["email"];
+                $password = $_POST["password"];
+                require_once "database.php";
+                $sql = "SELECT * FROM authors WHERE email = '$email'";
+                $result = mysqli_query($connect, $sql);
+                $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                if($user){
+                    if(password_verify($password, $user["password"])){
+                        header("Location: index.php");
+                        session_start();
+                        $_SESSION["user"] = "yes";
+                        die();
+                    }else{
+                        echo "<div class = 'alert alert-danger'>Password does not match.</div>"; 
+                    }
+                }
+                else{
+                    echo "<div class = 'alert alert-danger'>Email does not exist.</div>";
+                }
+            }
+        ?>
+
+        <form action = "login.php" method = "post">
+            <div class = "form-group">
+                <input type = "email" placeholder = "Enter email:" name ="email" class ="form-control">
+            </div>
+            <div class = "form-group">
+                <input type = "password" placeholder = "Enter password:" name ="password" class ="form-control">
+            </div>
+            <div class="form-btn">
+                <input type = "submit" value = "Login" name = "login" class = "btn btn-primary">
+            </div>
+        </form>
+        <div><p><br>Not registred yet? <a href="register.php"> Register here </a></p></div>
+    </div>
 </body>
 </html>
-
-<?php
-    if(isset($_POST["login"])){
-        if(!empty($_POST["email"] && !empty($_POST["password"]))){
-            $_SESSION["email"] = $_POST["email"];
-            $_SESSION["password"] = $_POST["password"];
-
-            echo $_SESSION["email"] . "<br>";
-            echo $_SESSION["password"] . "<br>";
-        }
-        else{
-            echo "Missing username/password";
-        }
-    }
-?>
--- register.php
+++ register.php
@@ -1,62 +1,108 @@
 <?php
-    include("database.php");
+session_start();
+if(isset($_SESSION["user"])){
+    header("Location: index.php");
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content ="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Register form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href = "style.css">
 </head>
 <body>
-    <form action ="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method = "post">
-        <h2> Welcome to BookApp </h2>
-        Name:<br>
-        <input type = "text" name= "name"> <br>
-        Surname:<br>
-        <input type = "text" name= "surname"> <br>
-        Email:<br>
-        <input type = "email" name= "email"> <br>
-        Password: <br>
-        <input type = "password" name = "password"> <br>
-        Admin privileges:
-        <input type = "checkbox" name = "is_admin"> <br>
-        <input type = "submit" name= "submit" value = "register">
-    </form>
+    <div class = "container">
+    <?php
+        if(isset($_POST["submit"])){
+            $name = $_POST["name"];
+            $surname = $_POST["surname"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $passwordRepeat = $_POST["repeat_password"];
+
+            if(isset($_POST["is_admin"])){
+                $is_admin = 1;
+            }
+            else{
+                $is_admin = 0;
+            }
+
+        
+            $passwordhash = password_hash($password, PASSWORD_DEFAULT);
+            $errors = array();
+        
+            if(empty($name) || empty($surname) || empty($email) || empty($password) || empty($passwordRepeat)){
+                array_push($errors, "All fields are required.");
+            }
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                array_push($errors, "Email is not valid");
+            }
+            if(strlen($password) < 8){
+                array_push($errors, "Password must be at least 8 characters long.");
+            }
+            if($password!== $passwordRepeat){
+                array_push($errors, "Password does not match.");
+            }
+            require_once "database.php";
+            $sql = "SELECT * FROM authors WHERE email = '$email'";
+            $result = mysqli_query($connect, $sql);
+            $rowCount = mysqli_num_rows($result);
+            if($rowCount>0){
+                array_push($errors, "Email already exists!");
+            }
+            if(count($errors) > 0){
+                foreach($errors as $error){
+                    echo "<div class='alert alert-danger'>$error</div>";
+                }
+            }
+            else{
+                
+                $sql = "INSERT INTO authors (name, surname, email, password, is_admin) VALUES(?, ?, ?, ?, ?)";
+                $stmt = mysqli_stmt_init($connect);
+                $preparestmt = mysqli_stmt_prepare($stmt, $sql);
+                if($preparestmt){
+                    mysqli_stmt_bind_param($stmt, "ssssi", $name, $surname, $email, $passwordhash, $is_admin);
+                    mysqli_stmt_execute($stmt);
+                    echo "<div class = 'alert alert-success'> You are registered succesfully.</div>";
+                }
+                else{
+                    die("Something went wrong");
+                }
+            
+            }
+        }
+        ?>
+        <form action = "register.php" method = "post">
+            <div class = "form-group">
+                <input type = "text" class = "form-control" name = "name" placeholder="name:">
+            </div>
+            <div class = "form-group">
+                <input type = "text" class = "form-control" name = "surname" placeholder="surname:">
+            </div>
+            <div class = "form-group">
+                <input type = "email" class = "form-control"name = "email" placeholder="email:">
+            </div>
+            <div class = "form-group">
+                <input type = "password" class = "form-control" name = "password" placeholder="password:">
+            </div>
+            <div class = "form-group">
+                <input type = "password" class = "form-control" name = "repeat_password" placeholder="repeat password:">
+            </div>
+            <div class = "form-btn">
+                Administrator privilages:
+                <input type = "checkbox" class ="btn btn-primary" value = "is_admin" name ="is_admin">
+            </div>
+            <div class = "form-btn">
+                <input type = "submit" class ="btn btn-primary" value = "Register" name ="submit">
+            </div>
+            
+        </from>
+        <div><p><br>Already registred? <a href="login.php">Login here </a></p></div>
+    </div>
 </body>
 </html>
-<?php 
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS);
-        $surname = filter_input(INPUT_POST, "surname", FILTER_SANITIZE_SPECIAL_CHARS);
-        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
-        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
-
-        if(empty($name)){
-            echo "Please enter a name.";
-        }
-        elseif(empty($surname)){
-            echo "Please enter a surname.";
-        }
-        elseif(empty($password)){
-            echo "Please enter a password.";
-        }
-        elseif(empty($email)){
-            echo "Please enter an email.";
-        }
-        else{
-            $hash = password_hash($password, PASSWORD_DEFAULT);
-            $sql = "INSERT INTO authors(name, surname, email,password) VALUES('$name' , '$surname', '$email', '$hash')";
-            
-            try{
-                mysqli_query($connect, $sql);
-                echo "You are now registered.";
-            }
-            catch(mysqli_sql_exception){
-                echo "That email already exists!";
-            }  
-        }
-    }
-
-    mysqli_close($connect);
-?>
